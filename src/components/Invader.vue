1a448e216b3d817be2789728a4103bcfe9d56fa3
mornir <jeromepott@hotmail.com>
2019-02-04 18:44:21 +0100

    First Commit

---
+++ src/components/Invader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white p-4 rounded-full mb-4">
+    <p class="text-3xl font-semibold text-center">{{ sentence }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sentence: {
+      type: String,
+      required: true,
+      default: 'Missing Sentence',
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="bg-blue-light min-h-screen">
+    <main class="container mx-auto p-4">
+      <Invader v-for="sentence in sentences"
+               :key="sentence"
+               :sentence="sentence" />
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import annyang from 'annyang'
+annyang.setLanguage('zh-CN')
 
+
+import Invader from '@/components/Invader'
+
+const sentences = ['我喜欢吃苹果', '他很慢', '他很忙']
 export default {
-  name: "home",
+  name: 'home',
+  data() {
+    return {
+      sentences,
+    }
+  },
   components: {
-    HelloWorld
-  }
-};
+    Invader,
+  },
+  mounted() {
+    if (annyang) {
+      // Let's define a command.
+      const commands = {
+        我喜欢吃苹果() {
+          console.log('我喜欢吃苹果!')
+        },
+        他很慢() {
+          console.log('他很慢!')
+        },
+       他很慢() {
+          console.log('他很慢!')
+        },
+        '他很忙': function() {
+          console.log('他很忙!')
+        },
+   
+      }
+
+      
+
+      // Add our commands to annyang
+      annyang.addCommands(commands)
+
+      // Start listening.
+      annyang.start({ continuous: false });
+    } else {
+      console.error('❌❌❌❌')
+    }
+  },
+}
 </script>
